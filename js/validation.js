9883d80e4c5dda52d4df568c25c4c4bcbebaff55
Lucas Sell Machado <75432770+Sellucas@users.noreply.github.com>
2022-09-07 14:00:31 -0300

    error message

--- js/validation.js
+++ js/validation.js
@@ -1,9 +1,6 @@
 function validateFields() {
-    const emailValid = isEmailValid();
-    document.getElementById('recover-form-btn').disabled = !emailValid;
-
-    const passwordValid = isPasswordValid();
-    document.getElementById('login-form-btn').disabled = !emailValid || !passwordValid;
+    toggleButtonsDisable();
+    toggleFormError();
 };
 
 function isEmailValid() {
@@ -12,15 +9,29 @@ function isEmailValid() {
         return false;
     }
     return validateEmail(email);
-}
+};
 
 function isPasswordValid() {
-    const password = document.querySelector('#password').value;
     if (!password) {
         return false;
     }
     return true;
-}
+};
+
+function toggleFormError() {
+    const email = document.querySelector('#email').value;
+    if (!email) {
+        document.getElementById('errorMessage-form').style.display = "block";
+    }
+};
+
+function toggleButtonsDisable() {
+    const emailValid = isEmailValid();
+    document.getElementById('recover-form-btn').disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById('login-form-btn').disabled = !emailValid || !passwordValid;
+};
 
 function validateEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
