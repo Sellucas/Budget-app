9b03b38aa577625d6cf6d0ccaa22e44491a41651
Lucas Sell Machado <75432770+Sellucas@users.noreply.github.com>
2022-09-16 12:12:35 -0300

    singUp and login firebase

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAbSJCNN8ckdQfqLlQl9XPLet17iTCN9J0",
+    authDomain: "budget-app-5110b.firebaseapp.com",
+    projectId: "budget-app-5110b",
+    storageBucket: "budget-app-5110b.appspot.com",
+    messagingSenderId: "897003291344",
+    appId: "1:897003291344:web:2c52abd29de14706e954d5"
+};
+firebase.initializeApp(firebaseConfig);
--- pages/signup/register.js
+++ pages/signup/register.js
@@ -26,6 +26,29 @@ function onChangeConfirmPassword() {
     toggleSignUpBtnDisable();
 };
 
+function signUp() {
+    showLoading();
+
+    const email = form.email().value;
+    const password = form.password().value;
+    firebase.auth().createUserWithEmailAndPassword(
+        email, password
+    ).then(() => {
+        hideLoading();
+        window.location.href = "../../pages/home/home.html"
+    }).catch(error => {
+        hideLoading();
+        alert(getErrorMessage(error));
+    })
+};
+
+function getErrorMessage(error) {
+    if (error.code == "auth/email-already-in-use") {
+        return "Email already in use";
+    }
+    return error.message;
+};
+
 function validatePasswordsMatch() {
     const confirmPassword = form.confirmPassword().value;
     const password = form.password().value;
